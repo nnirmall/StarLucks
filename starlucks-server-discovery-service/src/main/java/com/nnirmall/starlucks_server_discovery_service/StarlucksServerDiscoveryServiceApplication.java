a23e1d3fd466dacffd1f58d3c1edd1a1a34fed57
Nirmal Bhandari <vhandarinirmal@gmail.com>
2025-01-14 14:07:40 -0600

    initial discovery service commit

---
+++ starlucks-server-discovery-service/src/main/java/com/nnirmall/starlucks_server_discovery_service/StarlucksServerDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nnirmall.starlucks_server_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class StarlucksServerDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlucksServerDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ starlucks-server-discovery-service/src/test/java/com/nnirmall/starlucks_server_discovery_service/StarlucksServerDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnirmall.starlucks_server_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarlucksServerDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
